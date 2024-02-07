eac2916788d8d6d919e3ee4f66e2735fb30d9230
NATEX1 <111871403+NATEX1@users.noreply.github.com>
2024-02-07 10:50:02 +0700

    Update userRoute.js

--- routers/userRoute.js
+++ routers/userRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { verifyToken, verifyAdmin } = require("../middlewares/verifyToken");
-const { getAllUsers } = require("../controllers/userController");
+const { getAllUsers, deleteUser } = require("../controllers/userController");
 const router = express.Router();
 
 router.get("/", verifyToken, verifyAdmin, getAllUsers);
+router.delete("/:userId", verifyToken, verifyAdmin, deleteUser)
 
 module.exports = router;
