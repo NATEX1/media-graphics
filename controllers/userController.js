5f4ec225c349b3dc607fe8c985fd7c7e8bf2f763
NATEX1 <111871403+NATEX1@users.noreply.github.com>
2024-02-07 10:50:40 +0700

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,29 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.find();
     res.status(200).json(users);
   } catch (error) {
-    console.error('Error fetching users:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+
+    await User.findByIdAndDelete(userId);
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting user:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
 module.exports = {
   getAllUsers,
+  deleteUser
 };
